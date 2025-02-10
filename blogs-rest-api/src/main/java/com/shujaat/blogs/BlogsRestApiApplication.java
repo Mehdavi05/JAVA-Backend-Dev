964793acd2ae2f5addef6bd1e8a76f8f8309654b
Shujaat Hussain <shujaat.apple05@gmail.com>
2025-02-10 16:52:01 +0400

    Blogs REST API initial commit. Configurations for database connectivity added

---
+++ blogs-rest-api/src/main/java/com/shujaat/blogs/BlogsRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.shujaat.blogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogsRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogsRestApiApplication.class, args);
+	}
+
+}
