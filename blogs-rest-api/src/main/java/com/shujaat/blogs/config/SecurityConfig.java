a356136a3d1e62486d878465d206018b4758d2cf
Mehdavi05 <shujaat.apple05@gmail.com>
2025-02-12 11:56:43 +0400

    Securing REST API with Database Authentication

--- blogs-rest-api/src/main/java/com/shujaat/blogs/config/SecurityConfig.java
+++ blogs-rest-api/src/main/java/com/shujaat/blogs/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.shujaat.blogs.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -17,6 +19,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    private UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return  configuration.getAuthenticationManager();
+    }
 
     @Bean
     public static PasswordEncoder passwordEncoder(){
@@ -33,6 +46,7 @@ public class SecurityConfig {
         return http.build();
     }
 
+    /*
     @Bean
     UserDetailsService userDetailsService(){
         UserDetails user = User.builder()
@@ -49,4 +63,5 @@ public class SecurityConfig {
 
         return new InMemoryUserDetailsManager(user, admin);
     }
+     */
 }
