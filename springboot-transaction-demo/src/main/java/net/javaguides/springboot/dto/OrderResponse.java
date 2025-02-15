572168286118224985ca953062017f848d463e86
Shujaat Hussain <shujaat.apple05@gmail.com>
2025-02-15 22:11:16 +0400

    SpringBoot transaction demo

---
+++ springboot-transaction-demo/src/main/java/net/javaguides/springboot/dto/OrderResponse.java
@@ -0,0 +1,31 @@
+package net.javaguides.springboot.dto;
+
+public class OrderResponse {
+    private String orderTackingNumber;
+    private String status;
+    private String message;
+
+    public String getOrderTackingNumber() {
+        return orderTackingNumber;
+    }
+
+    public void setOrderTackingNumber(String orderTackingNumber) {
+        this.orderTackingNumber = orderTackingNumber;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ springboot-transaction-demo/src/main/java/net/javaguides/springboot/entity/Order.java
@@ -0,0 +1,108 @@
+package net.javaguides.springboot.entity;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderTackingNumber;
+    private int totalQuantity;
+    private BigDecimal totalPrice;
+    private String status;
+    @CreationTimestamp
+    private LocalDateTime dateCreated;
+    @UpdateTimestamp
+    private LocalDateTime lastUpdated;
+    private Long shoppingCartId;
+
+    public Order() {
+    }
+
+    public Order(Long id, String orderTackingNumber, int totalQuantity, BigDecimal totalPrice, String status, LocalDateTime dateCreated, LocalDateTime lastUpdated, Long shoppingCartId) {
+        this.id = id;
+        this.orderTackingNumber = orderTackingNumber;
+        this.totalQuantity = totalQuantity;
+        this.totalPrice = totalPrice;
+        this.status = status;
+        this.dateCreated = dateCreated;
+        this.lastUpdated = lastUpdated;
+        this.shoppingCartId = shoppingCartId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOrderTackingNumber() {
+        return orderTackingNumber;
+    }
+
+    public void setOrderTackingNumber(String orderTackingNumber) {
+        this.orderTackingNumber = orderTackingNumber;
+    }
+
+    public int getTotalQuantity() {
+        return totalQuantity;
+    }
+
+    public void setTotalQuantity(int totalQuantity) {
+        this.totalQuantity = totalQuantity;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDateTime dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public LocalDateTime getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(LocalDateTime lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public Long getShoppingCartId() {
+        return shoppingCartId;
+    }
+
+    public void setShoppingCartId(Long shoppingCartId) {
+        this.shoppingCartId = shoppingCartId;
+    }
+}
