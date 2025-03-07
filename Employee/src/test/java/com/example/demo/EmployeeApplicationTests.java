377871765b5e9577c75ee76b1a40d89924f25e4a
Shujaat Hussain <shujaat.apple05@gmail.com>
2025-03-07 09:53:22 +0400

    Updating annotations

--- Employee/src/test/java/com/example/demo/EmployeeApplicationTests.java
+++ Employee/src/test/java/com/example/demo/EmployeeApplicationTests.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import com.example.employeemanagement.Application;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+
+@SpringBootTest(classes = Application.class)
 class EmployeeApplicationTests {
 
 	@Test
--- blogs-rest-api/src/test/java/com/shujaat/blogs/BlogsRestApiApplicationTests.java
+++ blogs-rest-api/src/test/java/com/shujaat/blogs/BlogsRestApiApplicationTests.java
@@ -3,11 +3,10 @@ package com.shujaat.blogs;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BlogsRestApiApplication.class, properties = "spring.profiles.active=test")
 class BlogsRestApiApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
